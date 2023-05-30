461549a05dddb7ecf1f5fee9016dcc3cdff7b7a3
Gustavo Rodriguez Liccioni <gustavo.liccioni@weareadaptive.com>
2023-05-30 22:08:00 +0200

    fix: testing fail message

--- src/main/resources/db/migration/V5__element_parent_column.sql
+++
@@ -1,7 +0,0 @@
-alter table if exists element_entity
-    add column from_id bigint;
-
-alter table if exists element_entity
-    add constraint fk_element_entity_from_id
-        foreign key (from_id)
-            references element_entity;
